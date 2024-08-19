afa3c147b6317529891747879b443fad157be50c
Wilyendri <wilyendri@gmail.com>
2024-08-19 16:53:00 -0400

    Created gitignore

---
+++ PawsonalityApp.API/Models/Answer.cs
@@ -0,0 +1,18 @@
+
+namespace Pawsonality.API.Models;
+
+public class Answer 
+{
+    [key]
+    public int AnswerID {get; set;}
+
+    public string AnswerText {get; set;}
+
+    public string AnswerType {get; set;}
+
+    public int QuestionID {get; set;}
+
+    [ForeignKey("QuestionID")]
+    public Question? Question {get; set;}
+
+}
