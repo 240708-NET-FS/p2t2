607650ab85eae30741448d5eec9fd308c36a50d4
Wilyendri <wilyendri@gmail.com>
2024-08-23 20:25:15 -0400

    Fixed error on userService

--- PawsonalityApp.API/Services/Interfaces/IUserService.cs
+++ PawsonalityApp.API/Services/Interfaces/IUserService.cs
@@ -3,23 +3,22 @@ using Microsoft.AspNetCore.Identity;
 
 namespace PawsonalityApp.API.Services;
 
-public interface IUserService 
+public interface IUserService
 {
 
-        Task<IdentityResult> CreateUser(IdentityUser newUser);
+    Task<IdentityResult> CreateUser(IdentityUser newUser);
 
-        // Read
-        Task<IdentityUser> GetUserByID(string userID);
+    // Read
+    Task<IdentityUser> GetUserByID(string userID);
 
-        public Task<SignInResult> LoginUser(IdentityUser login);
+    public Task<SignInResult> LoginUser(string username, string password);
 
-        public Task LogoutUser();
+    public Task LogoutUser();
 
-        Task<ICollection<IdentityUser>> GetAllUsers();
+    Task<ICollection<IdentityUser>> GetAllUsers();
 
-        // Update
-        Task<IdentityUser> UpdateUser(string ID, IdentityUser userToUpdate);
+    Task<IdentityUser> UpdateUser(string ID, IdentityUser userToUpdate);
 
-        // Delete
-        Task<IdentityUser> DeleteUser(string ID);
+    // Delete
+    Task<IdentityUser> DeleteUser(string ID);
 }
