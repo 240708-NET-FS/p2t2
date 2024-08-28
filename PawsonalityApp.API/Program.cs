5698be0b744916cef282d9818471909cef007af3
Wilyendri <77631555+wilyendri@users.noreply.github.com>
2024-08-28 11:13:50 -0400

    Update Program.cs

--- PawsonalityApp.API/Program.cs
+++ PawsonalityApp.API/Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CORS", policy =>
     {
-        policy.WithOrigins("https://purple-desert-0b0e8ba1e.5.azurestaticapps.net", "https://purple-desert-0b0e8ba1e.5.azurestaticapps.net/login")  // Allow specific origin
+        policy.WithOrigins("https://purple-desert-0b0e8ba1e.5.azurestaticapps.net", "http://localhost:5173")  // Allow specific origin
               .AllowAnyMethod()  // Allow any HTTP method
               .AllowAnyHeader(); // Allow any header
     });
